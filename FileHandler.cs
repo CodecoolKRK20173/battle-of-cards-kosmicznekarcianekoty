543da4f7edd3637e2635ac40bcc6926c61c80224
ntiany <krolkas@gmail.com>
2020-03-09 19:54:12 +0100

    Add class for handling files' content

---
+++ FileHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Card_Game
+{
+    public static class FileHandler
+    {
+        public static List<string> GetFileContentAsList(string filePath)
+        {
+            StreamReader reader = new StreamReader(File.OpenRead(filePath));
+            List<string> data = new List<string>();
+            string line;
+
+            while (reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+                data.Add(line);
+            }
+
+            return data;
+        }
+    }
+}

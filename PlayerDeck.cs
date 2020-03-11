fc4095bc4de2282f99c349841a559958d190d793
Jakub Starczewski <jak.starczewski@gmail.com>
2020-03-11 19:50:16 +0100

    Add get/remove methods

--- PlayerDeck.cs
+++ PlayerDeck.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Card_Game
@@ -7,6 +8,21 @@ namespace Card_Game
         public Card GetTopCard()
         {
             return Cards.Last();
+        }
+        public void RemoveTopCard()
+        {
+            Cards.RemoveAt(Cards.Count -1);
+        }
+        public void AddCardsToDeckBottom(Card card)
+        {
+            Cards.Insert(0,card);
+        }
+        public void AddCardsToDeckBottom(List<Card> wonCards)
+        {
+            foreach(var card in wonCards)
+            {
+                Cards.Insert(0, card);
+            }
         }
     }
 }

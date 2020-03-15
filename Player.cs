e251cdcdd25634176501cb3ae90ad378330b199b
chash <chashafe@gmail.com>
2020-03-15 19:33:13 +0100

    commit before pulling

--- Player.cs
+++ Player.cs
@@ -5,8 +5,7 @@ namespace Card_Game
 {
     public class Player 
     {
-        bool isPlayerInHand = false;  // na true, jeśli prowadzi grę
-        bool isPlayerInGame = false; // na true, jeśli jeszcze w grze
+
         List<Card> Cards {get; set;}
         /*----*/
         public Player()
@@ -14,14 +13,9 @@ namespace Card_Game
             Cards = new List<Card>();
         }
         /*-----*/
-        public bool GetIsPlayerInHand() => isPlayerInHand; 
-        internal void ChangeIsPlayerInHand() => isPlayerInHand = true;
-
-        public bool GetIsPlayerInGame() => isPlayerInGame;
-        public void ChangeIsPlayerInGame() => isPlayerInGame = true;
 
         public void AddCardToPlayerCards(Card SingleCard) => Cards.Insert(0, SingleCard);
         public void RemoveCardFromePlayerCards(Card SingleCard) => new PlayerDeck().RemoveTopCard();
-        public bool CheckIfLoose(List<Card> Cards) => (Cards.Count < 1) ? true : false;
+
     }
 }

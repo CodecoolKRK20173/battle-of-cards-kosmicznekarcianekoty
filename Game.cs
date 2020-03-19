34ee27bcf8d1d4938210bbcd51ce24946cc4f6b4
ntiany <krolkas@gmail.com>
2020-03-19 15:39:37 +0100

    Add method to PlayRound

--- Game.cs
+++ Game.cs
@@ -6,11 +6,13 @@ namespace Card_Game
 {
     public class Game
     {
+        public bool isRunning { get; private set; }
         private Table gameTable;
         private int minPlayers = 2;
         private int maxPlayers = 5;
         private View view = new View();
         private string[] playersNames;
+        private int rounds = 1;
 
         public void IntroducePlayers()
         {
@@ -28,6 +30,44 @@ namespace Card_Game
         {
             gameTable = new Table(playersNames);
             view.Print($"Created {playersNames.Length} players");
+            gameTable.DealCards();
+            view.Print($"The cards are shuffled and dealed between players");
+        }
+
+        public void PlayRound()
+        {
+            view.Print($"Round: {rounds++}\nActive player: {gameTable.RoundWinner.Name}");
+            gameTable.PlayersPlayCard();
+            view.PrintCard(gameTable.GetActivePlayerCard(), gameTable.CardOwners);
+            view.Print("Please choose attribute to fight (1-4)");
+            view.PrintAttributes();
+            CardsAttributes attribute = GetAttribute();
+            view.Print($"{gameTable.RoundWinner.Name} plays {attribute}");
+        }
+
+        public CardsAttributes GetAttribute()
+        {
+            int attributesNumber = Enum.GetNames(typeof(CardsAttributes)).Length + 1;
+            bool correctInput = false;
+            string attribute;
+            int value = 0;
+            while (!correctInput)
+            {
+                attribute = Console.ReadLine();
+                try
+                {
+                    value = int.Parse(attribute);
+                }
+                catch
+                {
+                    view.Print("Incorrect input type!");
+                }
+                if (value < attributesNumber && value > 0) correctInput = true;
+                else view.Print("Incorrect attribute number!");
+            }
+
+            return (CardsAttributes)value-1;
+
         }
     }
 }

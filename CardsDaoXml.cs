9982fc54ef794a5e676eb4c597140f892404360a
ntiany <krolkas@gmail.com>
2020-03-21 11:08:11 +0100

    Add CardsDaoXml file without implementation

---
+++ CardsDaoXml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Card_Game
+{
+    class CardsDaoXml : ICardsDAO
+    {
+        public Card GetCardByID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Card> GetCards()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
